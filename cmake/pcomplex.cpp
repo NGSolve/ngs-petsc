2458e83f7a13cd9536763366f3ec7b8b61afdd45
Lukas <lukas.kogler@tuwien.ac.at>
2020-08-05 11:13:51 +0200

    proper check for complex petsc

---
+++ cmake/pcomplex.cpp
@@ -0,0 +1,9 @@
+// if this compiles, the PETSc installation is complex
+
+#include "petsc.h"
+
+#ifndef PETSC_USE_COMPLEX
+aifjalkcnmsckm
+#endif
+
+int main () { return 0; }
---
+++ cmake/plongind.cpp
@@ -0,0 +1,9 @@
+// if this compiles, the PETSc installation uses 64 bit indices
+
+#include "petsc.h"
+
+#ifndef PETSC_USE_64BIT_INDICES
+aihsflakcnskn
+#endif
+
+int main () { return 0; }
